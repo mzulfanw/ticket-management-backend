6a21cd0c735325cb26c9229b66cb13ee29f90360
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-08-02 02:01:35 +0700

    fix(logger): fixed cannot read dirname

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,9 +1,14 @@
 import morgan from "morgan"
 import fs from "fs"
 import path from "path"
+import { fileURLToPath } from "url"
 import type { RequestHandler } from "express"
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const logDirectory = path.join(__dirname, '../../logs');
+
 
 if (!fs.existsSync(logDirectory)) {
   fs.mkdirSync(logDirectory)
